3e46633ca453c4f51f0654e1e6ac58334890a40d
LonelyMinotaur7 <LonelyMinotaur7@gmail.com>
2023-10-19 12:47:23 -0400

    Random Spawn Pos

--- Assets/Scripts/Candy/Candy 1.cs
+++ Assets/Scripts/Candy/Candy 1.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Candy1 : MonoBehaviour
 {
 
     [SerializeField] private Transform Spawn;
+    [SerializeField] private GameObject SpawnObj;
+    [SerializeField] private Vector2 SpawnVec;
     [SerializeField] private Rigidbody2D CandyA;
     [SerializeField] private Rigidbody2D CandyB;
     [SerializeField] private Rigidbody2D CandyC;
@@ -18,11 +21,13 @@ public class Candy1 : MonoBehaviour
     public float Regen;
     public int RandomSpawn;
     public int RandomCandySpawn;
+    public float RandomSpawnSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
         RandomSpawn = Random.Range(0, 10);
+        
     }
 
     // Update is called once per frame
@@ -33,8 +38,11 @@ public class Candy1 : MonoBehaviour
         if (TimeLeft < RandomSpawn)
         {
            RandomCandySpawn = Random.Range(1, 6);
-            TimeLeft = 13;
-            RandomSpawn = Random.Range(0, 10);
+            TimeLeft = 11;
+            RandomSpawn = Random.Range(8, 10);
+            RandomSpawnSpawn = Random.Range(-8, 8);
+            SpawnVec = new Vector2(RandomSpawnSpawn, 5.7f);
+            SpawnObj.transform.position = SpawnVec;
             if (RandomCandySpawn == 1 )
             {
                 Rigidbody2D currentProj = Instantiate(CandyA, Spawn.position, Quaternion.identity);
